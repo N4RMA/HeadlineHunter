87ba0d9149cf100d6401bff35601c1459104497a
N4RMA <manar-yacine@outlook.com>
2023-04-15 19:27:07 -0400

    added some error handling

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Send a GET request to the Packet Storm Security news page
-url = "https://packetstormsecurity.com/news/"
-response = requests.get(url)
 
-# Parse the HTML content of the news page
-soup = BeautifulSoup(response.content, 'html.parser')
+try:
+    # Send a GET request to the Packet Storm Security news page
+    url = "https://packetstormsecurity.com/news/"
+    response = requests.get(url)
+    response.raise_for_status()  # check if the request is successful
 
-# Extract the news items from the page
-news_items = soup.find_all('dl')
+    # Parse the HTML content of the news page
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the headline and date from each news item and print them to the console
-for news_item in news_items:
-    headline = news_item.find('a', href=True, text=True)
-    date = news_item.find('dd', class_='datetime').find('a').text
-    if headline and date:
-        print(f"Headline: {headline.text.strip()}")
-        print(f"Date: {date}\n")
+    # Extract the news items from the page
+    news_items = soup.find_all('dl')
+
+    # Extract the headline and date from each news item and log them using the logging module
+    for news_item in news_items:
+        try:
+            headline = news_item.find('a', href=True, text=True)
+            date = news_item.find('dd', class_='datetime').find('a').text
+            if headline and date:
+                print(f"Headline: {headline.text.strip()}")
+                print(f"Date: {date}")
+        except AttributeError as e:
+            print(f"Failed to extract headline or date: {e}")
+
+except requests.exceptions.RequestException as e:
+    # Log the error and raise it again
+    print(f"Failed to make request: {e}")
+    raise e
 
