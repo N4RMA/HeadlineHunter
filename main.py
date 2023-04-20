bff5603f581c69c52cabe6f59aae1fc8988ed517
N4RMA <manar-yacine@outlook.com>
2023-04-20 18:36:22 -0400

    added comments

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import requests
 import logging
 from bs4 import BeautifulSoup
 
+# Initialize logging configuration
 logging.basicConfig(filename='scraper.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
 
-
+# Define a function for scraping news from Packet Storm Security
 def packetstormsecurity_scraper():
     news = []
     try:
         # Send a GET request to the Packet Storm Security news page
         url = "https://packetstormsecurity.com/news/"
         response = requests.get(url)
-        response.raise_for_status()  # check if the request is successful
+        response.raise_for_status()  # Check if the request is successful
 
-        # Parse the HTML content of the news page
+        # Parse the HTML content of the news page using BeautifulSoup
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Extract the news items from the page
@@ -29,14 +30,16 @@ def packetstormsecurity_scraper():
                     news.append(news_item)
                     logging.info(f"{headline.text.strip()}, {date}")
             except AttributeError as e:
+                # Log the error if there is any issue in extracting the headline or date from news items
                 logging.error(f"Failed to extract headline or date: {e}")
 
     except requests.exceptions.RequestException as e:
-        # Log the error and raise it again
+        # Log the error if there is any issue in making a request to the news page and raise it again
         logging.error(f"Failed to make request: {e}")
         raise e
 
     return news
 
-print(packetstormsecurity_scraper()[0]['headline'])
-print(packetstormsecurity_scraper()[0]['date'])
+
+# print(packetstormsecurity_scraper()[0]['headline'])
+# print(packetstormsecurity_scraper()[0]['date'])
