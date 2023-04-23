9a06496c18b608f0289f34cfd804cdccca831ce2
N4RMA <manar-yacine@outlook.com>
2023-04-23 18:58:16 -0400

    added typing module | added comments

--- selenium_utils.py
+++ selenium_utils.py
@@ -1,19 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from typing import Any
 
-def get_page_source(url):
-    # Set up Selenium options to run headless (without a GUI)
-    options = Options()
-    options.headless = True
+def get_page_source(url: str) -> str:
+    """
+    Returns the page source of the given URL using a headless Chrome browser.
 
-    # Set up Chrome WebDriver with the options
-    driver = webdriver.Chrome(options=options)
+    Args:
+        url (str): The URL to retrieve the page source from.
 
-    # Request the webpage and retrieve its source code
-    driver.get(url)
-    page_source = driver.page_source
+    Returns:
+        str: The page source of the given URL.
 
-    # Close the WebDriver and return the page source
-    driver.quit()
-    return page_source
+    Raises:
+        Exception: If the page source cannot be retrieved.
+    """
+    try:
+        # Set up Selenium options to run headless (without a GUI)
+        options = Options()
+        options.headless = True
 
+        # Set up Chrome WebDriver with the options
+        driver = webdriver.Chrome(options=options)
+
+        # Request the webpage and retrieve its source code
+        driver.get(url)
+        page_source = driver.page_source
+
+        # Close the WebDriver and return the page source
+        driver.quit()
+        return page_source
+
+    except Exception as e:
+        raise Exception(f"Error retrieving page source from {url}: {e}")
