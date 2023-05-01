65da9cc7bbb086008355c2c0f2821933a86ae8ba
N4RMA <manar-yacine@outlook.com>
2023-04-30 22:20:26 -0400

    fixed missing import

--- web_scrapers/theregister.py
+++ web_scrapers/theregister.py
@@ -1,23 +1,17 @@
 import logging
 import requests
+from bs4 import BeautifulSoup
 from utils.selenium_utils import get_page_source
 
 
 def theregister_scraper():
     # Make a request to the page you want to scrape
     url = "https://theregister.com"
-    return get_page_source(url)
+    page_src = get_page_source(url)
     
     # Parse the HTML content of the page using Beautiful Soup
-    # soup = BeautifulSoup(response.content, "html.parser")
-
-    # Find all the <article> tags on the page and extract the text inside them
-    # articles = soup.find_all("article")
-    # print(articles)
-    # for article in articles:
-    #     article_text = article.text.strip()  # get the text inside the article tag
-    #     print(article_text)  # print the text inside the article tag
-
+    soup = BeautifulSoup(page_src, "html.parser")
+    return page_src
 
 
 print(theregister_scraper())
