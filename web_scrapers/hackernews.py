9371523b63a88677c1d0b1c09e665861750b3415
N4RMA <manar-yacine@outlook.com>
2023-05-04 17:58:16 -0400

    added function to convert data to json

--- web_scrapers/hackernews.py
+++ web_scrapers/hackernews.py
@@ -1,16 +1,39 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
+def parse_article(element):
+    data = {}
+
+    # Extract title
+    title = element.find('h2', {'class': 'home-title'})
+    if title:
+        data["title"] = title.text.strip()
+
+    # Extract date and category
+    label = element.find('div', {'class': 'item-label'})
+    if label:
+        date_and_category = label.find_all('span')
+        if len(date_and_category) == 2:
+            data["date"] = date_and_category[0].text.strip()
+            data["category"] = date_and_category[1].text.strip()
+
+    # Extract summary
+    summary = element.find('div', {'class': 'home-desc'})
+    if summary:
+        data["summary"] = summary.text.strip()
+
+    return json.dumps(data, indent=4)
 
 
 def hackernews_scraper():
-    # URL of the Hacker News homepage
     url = 'https://thehackernews.com/'
     page_src = requests.get(url).content
     soup = BeautifulSoup(page_src, 'html.parser')
     elements = soup.find_all('div', {'class': 'clear home-right'})
     for element in elements:
-        print(element.text)
-
+        json_data = parse_article(element)
+        print(json_data)
+        break
 
 hackernews_scraper()
